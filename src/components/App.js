759a711c75062991973d94aeaeec440f40db703b
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-07-17 13:14:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
-
+  const [text, setText] = useState(");
+  const [wordCount, setCount] = useState(0);
+  const [charCount, setCharCount] = useState(0);
+  const [charLimit, setCharLimit] = useState(50);
+  const [fontSize, setFontSize] = useState(16);
+  
+  const handleChange = (e) => {
+  const value = e.target.value;
+  if(value.length <= charLimit){
+  setText(value);
+  }
+  }
   return (
     <div id="main">
     </div>
